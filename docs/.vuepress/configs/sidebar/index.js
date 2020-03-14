0ea7722ad66d7d96784d328c329b03c06fa1f603
yesixuan <18373209286@163.com>
2020-03-14 21:44:23 +0800

    阿纳克萨格拉斯

--- docs/.vuepress/configs/sidebar/index.js
+++ docs/.vuepress/configs/sidebar/index.js
@@ -4,5 +4,6 @@ module.exports = {
     'history-of-philosophy',
     'origin-of-philosophy',
     'materialism-and-immaterialism',
+    'atomism&sophists',
   ],
 }
--- docs/.vuepress/configs/theme/index.js
+++ docs/.vuepress/configs/theme/index.js
@@ -3,12 +3,11 @@ const themeReco = require('./themeReco.js')
 const sidebar = require('../sidebar/')
 // const locales = require('../locales/')
 const nav = require('../nav')
-
 module.exports = Object.assign({}, themeReco, {
   head: [
     ['meta', {
       name: 'viewport',
-      content: 'width=device-width,initial-scale=1,user-scalable=no'
+      content: 'width=device-width, initial-scale=1.0,minimum-scale=1.0, maximum-scale=1.0, user-scalable==no'
     }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
