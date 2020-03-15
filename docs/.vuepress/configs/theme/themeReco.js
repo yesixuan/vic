9d73e88defbe8c8bb6797909fbd48d6761701114
yesixuan <18373209286@163.com>
2020-03-15 19:23:18 +0800

    德谟克里特

--- docs/.vuepress/configs/theme/themeReco.js
+++ docs/.vuepress/configs/theme/themeReco.js
@@ -9,7 +9,7 @@ module.exports = {
   // 侧边栏深度
   sidebarDepth: 1,
   // 最后更新时间
-  lastUpdated: '上次更新', // string | boolean
+  lastUpdated: false, // string | boolean
   // 作者
   author: 'Vic',
   // authorAvatar: '/head.png',
@@ -18,20 +18,20 @@ module.exports = {
   // 项目开始时间
   startYear: '2020',
   // valine 设置
-  // valineConfig: {
-  //   appId: 'jvc9s4BkJYQNOcpsbVTPMePe-gzGzoHsz',
-  //   appKey: 'Js91M9DfM9vPwVaUj7xdkbxh',
-  //   placeholder: '填写邮箱可以收到回复提醒哦！',
-  //   notify: true,
-  //   recordIP: true
-  // },
-  vssueConfig: {
-    platform: 'github',
-    owner: 'yesixuan',
-    repo: 'vic',
-    clientId: '43a8e5d8ab5db4be4c93',
-    clientSecret: 'c1bb3d31de3b99540a2705b081c073dfc4191979'
+  valineConfig: {
+    appId: 'jvc9s4BkJYQNOcpsbVTPMePe-gzGzoHsz',
+    appKey: 'Js91M9DfM9vPwVaUj7xdkbxh',
+    placeholder: '填写邮箱可以收到回复提醒哦！',
+    notify: true,
+    recordIP: true
   },
+  // vssueConfig: {
+  //   platform: 'github',
+  //   owner: 'yesixuan',
+  //   repo: 'vic',
+  //   clientId: '43a8e5d8ab5db4be4c93',
+  //   clientSecret: 'c1bb3d31de3b99540a2705b081c073dfc4191979'
+  // },
   // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
   repo: 'https://www.github.com/yesixuan/vic',
   // // 假如文档不是放在仓库的根目录下：
@@ -39,7 +39,7 @@ module.exports = {
   // // 假如文档放在一个特定的分支下：
   docsBranch: 'master',
   // // 默认是 false, 设置为 true 来启用
-  editLinks: true
+  editLinks: false,
 }
 
 // github issue 评论
